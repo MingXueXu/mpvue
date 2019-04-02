277e66e689f643035109e45a2f8ba4f495378dc6
MingXueXu <1687651884@qq.com>
2019-04-02 21:45:16 +0800

    index

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -3,12 +3,17 @@
      <i-notice-bar icon="systemprompt" loop>
     今日新书：钱钟书——《围城》
     </i-notice-bar>
-     <view class="userinfo">
+    <i-input type="text" title="搜索" placeholder="请输入书籍名称 关键字" />
+    <view class="userinfo">
     <view class="userinfo-avatar">
     <open-data type="userAvatarUrl"></open-data>
     </view>
     <open-data type="userNickName"></open-data>
     </view>
+    <i-row>
+    <i-col span="12" i-class="col-class"><i-icon type="activity" size="28" color="#1296db" />签到中心</i-col>
+    <i-col span="12" i-class="col-class ">当前等级:LV3<i-icon type="collection" size="28" color="#1296db" /></i-col>
+</i-row>
     <i-grid i-class="no-border">
     <i-grid-item i-class="no-border">
         <i-grid-icon>
@@ -20,36 +25,43 @@
         <i-grid-icon>
             <image src="/static/grid/2.png" />
         </i-grid-icon>
-        <i-grid-label>订阅期刊</i-grid-label>
+        <i-grid-label>我的期刊</i-grid-label>
     </i-grid-item>
     <i-grid-item i-class="no-border">
         <i-grid-icon>
             <image src="/static/grid/3.png" />
         </i-grid-icon>
-        <i-grid-label>在线预订</i-grid-label>
+        <i-grid-label>我的收藏</i-grid-label>
+    </i-grid-item>
+    <i-grid-item i-class="no-border">
+        <i-grid-icon>
+            <image src="/static/grid/活动.png" />
+        </i-grid-icon>
+        <i-grid-label>我的活动</i-grid-label>
     </i-grid-item>
 </i-grid>
-<i-grid i-class="no-border">
-    <i-grid-item i-class="no-border">
-        <i-grid-icon>
-            <image src="/static/grid/1.png" />
-        </i-grid-icon>
-        <i-grid-label>我的书架</i-grid-label>
-    </i-grid-item>
-    <i-grid-item i-class="no-border">
-        <i-grid-icon>
-            <image src="/static/grid/2.png" />
-        </i-grid-icon>
-        <i-grid-label>订阅期刊</i-grid-label>
-    </i-grid-item>
-    <i-grid-item i-class="no-border">
-        <i-grid-icon>
-            <image src="/static/grid/3.png" />
-        </i-grid-icon>
-        <i-grid-label>在线预订</i-grid-label>
-    </i-grid-item>
-</i-grid>
-  </div>
+<i-panel title="每日排行">
+      <view class="top-padding"></view>
+      <i-card  i-class="top-padding" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+        <view slot="content">snow</view>
+        <view slot="footer">NO.1</view>
+      </i-card>
+</i-panel>
+<i-panel title="每日推荐">
+      <view class="top-padding"></view>
+      <i-card i-class="top-padding" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+        <view slot="content">明朝那些事</view>
+        <view slot="footer">评分：9.5</view>
+      </i-card>
+</i-panel>
+<i-panel title="图书导购">
+      <view class="top-padding"></view>
+      <i-card i-class="top-padding" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+        <view slot="content">内容不错</view>
+        <view slot="footer">尾部内容</view>
+      </i-card>
+      </i-panel>
+   </div>
 </template>
 
 <script>
@@ -95,6 +107,9 @@ export default {
 div >>> .no-border {
   border-width: 0pt;
 }
+.top-padding {
+  padding-top: 50rpx;
+}
 .userinfo {
 position: relative;
 width: 750rpx;
@@ -115,5 +130,8 @@ margin-top: 50rpx;
 border-radius: 50%;
 border: 2px solid #fff;
 box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.2);
+}
+div>>>.col-class{
+  padding-right: 0pt;
 }
 </style>
---
+++ src/pages/set/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <i-panel title="信息推送设置">
+    <view style="padding: 15px;">
+      <i-cell-group>
+    <i-cell title="基本用法">
+        <i-switch  @change="onChange" slot="footer"></i-switch>
+    </i-cell>
+    <i-cell title="自定义内容">
+        <i-switch  size="large" @change="onChange" slot="footer">
+            <view slot="open">开启</view>
+            <view slot="close">关闭</view>
+        </i-switch>
+    </i-cell>
+    <i-cell title="图标">
+        <i-switch @change="onChange" slot="footer">
+            <i-icon type="right" slot="open"></i-icon>
+            <i-icon type="close" slot="close"></i-icon>
+        </i-switch>
+    </i-cell>
+</i-cell-group></view>
+</i-panel>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  data: {
+     switch1 : false
+  },
+  methods: {
+    onChange(event){
+        const detail = event.detail;
+        this.setData({
+            'switch1' : detail.value
+        })
+        
+    },
+  }
+}
+</script>
+
+<style>
+.counter-warp {
+  text-align: center;
+  margin-top: 100px;
+}
+.home {
+  display: inline-block;
+  margin: 100px auto;
+  padding: 5px 10px;
+  color: blue;
+  border: 1px solid blue;
+}
+</style>
