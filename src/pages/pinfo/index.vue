103c2c90b2b29aaacc116e6b56983984f5ea5e08
student <student@hubu.edu.cn>
2019-05-29 11:35:31 +0800

    info

--- src/pages/pinfo/index.vue
+++ src/pages/pinfo/index.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
  <i-panel title="读者基本信息">
-    <i-input :value="value1" title="姓名" autofocus placeholder="请输入名字" />
+    <i-input :value="value1"  title="姓名" autofocus placeholder="请输入名字" />
     <i-input :value="value2" type="number" title="学号" placeholder="请输入学号" />
     <i-input :value="value3 " title="年级专业" placeholder="请输入你的年级和专业" />
 </i-panel>
@@ -11,7 +11,7 @@
 </i-panel>
 
 <i-panel title="用户修改">
-  <i-input :value="value1" title="姓名" autofocus placeholder="请输入名字" />
+  <i-input :value="value1" @change="changeName($event)"  title="姓名" autofocus placeholder="请输入名字" />
     <i-input :value="value2" type="number" title="学号" placeholder="请输入学号" />
     <i-input :value="value3 " title="年级专业" placeholder="请输入你的年级和专业" />
     <i-input :value="value2" type="password" title="密码" placeholder="请输入新密码" />
@@ -34,9 +34,9 @@ export default {
 
   data () {
     return {
-     value1: '',
-        value2: '',
-        value3: '',
+     value1: '许明雪',
+        value2: '201722111112064',
+        value3: '闪光少女',
         value4: '输入框已禁用',
         value5: '',
         value6: '',
@@ -44,11 +44,16 @@ export default {
   }
   },
   methods:{
-       handleChange (detail) {
-        this.setData({
-            current: detail.key
-        });
+      changeName (event) {
+        console.log(event)
+      if(event.target.detail.value==0){
+      wx.showToast({
+        title:"姓名不能为空",
+        icon:'none'
+         })
+      }
     },
+    
   },
   created () {
     
